701d0f8c63a3ebb71100cde176da23330bf3dc3f
graemet <graeme.thornber@dna.co.nz>
2016-12-20 11:36:20 +1300

    Add JS resize function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-	console.log('test');
+	
+	//Run function on resize
+	var timeoutid;
+	$(window).resize(function() {
+		clearTimeout(timeoutid);
+		timeoutid = setTimeout(doneResizing, 333);
+	});
 });
+
+function doneResizing(){
+	console.log('resize happened');
+}
